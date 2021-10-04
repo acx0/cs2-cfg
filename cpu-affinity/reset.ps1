902daa32332910fd0795213e73a7193f16379efa
Sam Lidder <sam.lidder@gmail.com>
2021-10-03 22:46:52 -0400

    add affinity reset script

---
+++ cpu-affinity/reset.ps1
@@ -0,0 +1,9 @@
+# note: run as admin to pin system processes too
+
+foreach ($process in Get-Process) {
+    if ($process.ProcessorAffinity -eq $null) {
+        continue
+    }
+
+    $process.ProcessorAffinity = [Convert]::ToInt64("FFFF", 16)
+}
